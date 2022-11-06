a2c50ffa65c58807352916f6bbd65d091e164f20
Muhammad Rizwan Ali <56793250+razi236@users.noreply.github.com>
2022-11-06 21:39:16 +0100

    Integration of SQL-Lite data base.

--- frontend/src/main/java/org/abs_models/backend/prettyprint/PrettyPrinterBackEnd.java
+++ frontend/src/main/java/org/abs_models/backend/prettyprint/PrettyPrinterBackEnd.java
@@ -9,11 +9,17 @@ import java.io.*;
 import org.abs_models.Absc;
 import org.abs_models.frontend.ast.Model;
 import org.abs_models.frontend.parser.Main;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class PrettyPrinterBackEnd extends Main {
 
     public static int doMain(Absc args) {
         PrettyPrinterBackEnd backend = new PrettyPrinterBackEnd();
+        //backend.selectAll();
         backend.arguments = args;
         int result = 0;
         try {
@@ -26,6 +32,37 @@ public class PrettyPrinterBackEnd extends Main {
             result = 1;
         }
         return result;
+    }
+
+    public Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:identifier.sqlite";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public void selectAll(){
+        String sql = "SELECT * FROM Resources";
+
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("ResourceID") +  "\t" +
+                    rs.getString("ResourceCategory") + "\t" +
+                    rs.getInt("ResourceEfficiency"));
+            }
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public int compile(Absc args) throws Exception {
@@ -69,10 +106,42 @@ public class PrettyPrinterBackEnd extends Main {
         model.doPrettyPrint(writer, formatter);*/
 
         File file = new File("RABS.abs");
-        PrintWriter writer = null;
-        /*FileInputStream inputStream = new FileInputStream("ResourceManagerAPI.abs");
-        FileOutputStream outputStream = new FileOutputStream(file);
-        PrintWriter writer = null;
+        PrintWriter writer = new PrintWriter(file);
+        writer.print("module ABS.ResourceManager;");
+        writer.println();
+        writer.print("import * from ABS.StdLib;");
+        writer.println();
+        writer.print("export *;");
+        writer.println();
+        writer.print("data Quality = ");
+
+        String sql = "SELECT * FROM ResourceQuality";
+        Connection conn = this.connect();
+        try (
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+             if(rs.next()){
+                 writer.print(rs.getString("Quality"));
+             }
+
+            // loop through the result set
+            while (rs.next()) {
+                writer.print(" | ");
+                writer.print(rs.getString("Quality"));
+            }
+            writer.print(";");
+            writer.println();
+            writer.println();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            writer.close();
+            conn.close();
+        }
+        FileInputStream inputStream = new FileInputStream("frontend/src/main/resources/abs/lang/ResourceManager.abs");
+        FileOutputStream outputStream = new FileOutputStream(file,true);
+        //PrintWriter writer = null;
         try {
             // declare variable for indexing
             int i;
@@ -96,11 +165,11 @@ public class PrettyPrinterBackEnd extends Main {
             if (outputStream != null) {
                 outputStream.close();
             }
-        }*/
+        }
         BufferedWriter br = null;
         FileWriter fr = null;
         try {
-            fr = new FileWriter(file);
+            fr = new FileWriter(file,true);
             br = new BufferedWriter(fr);
             writer = new PrintWriter(br);
             System.setProperty("line.separator", System.lineSeparator());
