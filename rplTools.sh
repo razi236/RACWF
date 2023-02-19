1c0fd2a4dad22d8f3c2142ab9b89618441f427b6
Muhammad Rizwan Ali <56793250+razi236@users.noreply.github.com>
2023-02-19 22:44:52 +0100

    RplTools

--- rplTools.sh
+++ rplTools.sh
@@ -6,47 +6,103 @@ echo "Press 1 for Simulation"
 echo "Press 2 for Cost Analysis"
 echo "Press 3 for RPL to ABS translation"
 read option
-if [ $option = "1" ]
-then
-{
-  echo "Please enter the filename:"
-  read file
-  start=`echo $(($(gdate +%s%N)/1000000))`
-  frontend/bin/absc -s ./examples/$file
-  cp RABS.abs ABS.rpl
-  frontend/bin/absc -e ABS.rpl
-  gen/erl/run
-  end=`echo $(($(gdate +%s%N)/1000000))`
-  echo Execution time was `expr $end - $start` mili seconds.
-}
-# shellcheck disable=SC1073
-elif [ $option = "2" ]
-then
-{
-  echo "Please enter the filename:"
-  read file
-  start=`echo $(($(gdate +%s%N)/1000000))`
-  frontend/bin/absc -c ./examples/$file
-  end=`echo $(($(gdate +%s%N)/1000000))`
-  echo Execution time was `expr $end - $start` mili seconds.
-  grep -o '\bmax\b' CostEquations.txt | wc -l
-}
-# shellcheck disable=SC1131
-elif [ $option = "3" ]
-then
-{
-  echo "Please enter the filename:"
-  read file
-  start=`echo $(($(gdate +%s%N)/1000000))`
-  frontend/bin/absc -t ./examples/$file
-  end=`echo $(($(gdate +%s%N)/1000000))`
-  echo Execution time was `expr $end - $start` mili seconds.
-}
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    {
+    if [ $option = "1" ]
+    then
+    {
+      echo "Please enter the filename:"
+      read file
+      start=`echo $(($(gdate +%s%N)/1000000))`
+      frontend/bin/absc -s ./examples/$file
+      cp RABS.abs ABS.rpl
+      frontend/bin/absc -e ABS.rpl
+      gen/erl/run
+      end=`echo $(($(gdate +%s%N)/1000000))`
+      echo Execution time was `expr $end - $start` mili seconds.
+    }
+    # shellcheck disable=SC1073
+    elif [ $option = "2" ]
+    then
+    {
+      echo "Please enter the filename:"
+      read file
+      start=`echo $(($(gdate +%s%N)/1000000))`
+      frontend/bin/absc -c ./examples/$file
+      end=`echo $(($(gdate +%s%N)/1000000))`
+      echo Execution time was `expr $end - $start` mili seconds.
+      grep -o '\bmax\b' CostEquations.txt | wc -l
+    }
+    # shellcheck disable=SC1131
+    elif [ $option = "3" ]
+    then
+    {
+      echo "Please enter the filename:"
+      read file
+      start=`echo $(($(gdate +%s%N)/1000000))`
+      frontend/bin/absc -t ./examples/$file
+      end=`echo $(($(gdate +%s%N)/1000000))`
+      echo Execution time was `expr $end - $start` mili seconds.
+    }
+    else
+    {
+        echo "Wrong selection"
+
+    }
+    fi
+    }
 else
-{
-    echo "Wrong selection"
+    {
+    if [ $option = "1" ]
+    then
+    {
+      echo "Please enter the filename:"
+      read file
+      start=`echo $($(date +%s))`
+      frontend/bin/absc -s ./examples/$file
+      cp RABS.abs ABS.rpl
+      frontend/bin/absc -e ABS.rpl
+      gen/erl/run
+      end=`echo $($(date +%s))`
+      echo Execution time was `expr $end - $start` seconds.
+    }
+    # shellcheck disable=SC1073
+    elif [ $option = "2" ]
+    then
+    {
+        echo "Please enter the filename:"
+        read file
+        start=`echo $($(date +%s))`
+        frontend/bin/absc -s ./examples/$file
+        cp RABS.abs ABS.rpl
+        frontend/bin/absc -e ABS.rpl
+        gen/erl/run
+        end=`echo $($(date +%s))`
+        echo Execution time was `expr $end - $start` seconds.
+        grep -o '\bmax\b' CostEquations.txt | wc -l
+    }
+    # shellcheck disable=SC1131
+    elif [ $option = "3" ]
+    then
+    {
+        echo "Please enter the filename:"
+        read file
+        start=`echo $($(date +%s))`
+        frontend/bin/absc -s ./examples/$file
+        cp RABS.abs ABS.rpl
+        frontend/bin/absc -e ABS.rpl
+        gen/erl/run
+        end=`echo $($(date +%s))`
+        echo Execution time was `expr $end - $start` seconds.
+    }
+    else
+    {
+        echo "Wrong selection"
 
-}
-
+    }
+    fi
+    }
 fi
+
+
 
